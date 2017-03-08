695e98fcc91f4b4e8024764972976f83df45b33d
Maarja <Maarja>
2017-03-08 13:02:57 +0200

    Panime failid juurde php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$date = "08.03.2017";
+$number =5;
+$float=5.2;
+$float1=5.18;
+
+
+echo "Täna on  08.03 2017, kolmas tund<br>";
+echo "Esimene float on ".$float." jat teine float on ".$float1."<br>";
+echo $float + $float1;
+
+?>
