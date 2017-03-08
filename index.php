baa70017dfc3acc4e1737e7fff52f45f2b37f4c8
Maarja <Maarja>
2017-03-08 13:25:25 +0200

    Kellaaja lisamine

--- index.php
+++ index.php
@@ -8,6 +8,22 @@ $float1=5.18;
 
 echo "Täna on  08.03 2017, kolmas tund<br>";
 echo "Esimene float on ".$float." jat teine float on ".$float1."<br>";
-echo $float + $float1;
+echo $float + $float1."<br>";
+
+function aeg(){
+    date_default_timezone_set("Europe/Tallinn");
+    $time =date("H:i:s");
+    echo $time."<br>";
+    
+    if($time>"16:00"){
+        echo "Tund läbi";
+    }else{
+        echo "Tund kestab";
+    }
+
+}
+aeg();
+
+
 
 ?>
